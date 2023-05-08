ea842e69ae570dec86d3dff4f31445b820b5a7db
Businge931 <jimmybusinge9@gmail.com>
2023-05-08 03:29:58 +0300

    feat: setup navbar

---
+++ app/components/Avator.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+
+const Avator = () => {
+  return (
+    <Image
+      alt="Avator"
+      src="/images/placeholder.jpg"
+      className="rounded-full"
+      height="30"
+      width="30"
+    />
+  );
+};
+
+export default Avator;
