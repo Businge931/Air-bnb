93f568c767a7f2b3c2b3a7af8c0e8e746729d970
Businge931 <jimmybusinge9@gmail.com>
2023-05-08 06:36:36 +0300

    feat: added modal component

--- app/components/Modals/Modal.tsx
+++ app/components/Modals/Modal.tsx
@@ -80,7 +80,7 @@ const Modal: React.FC<ModalProps> = ({
               {/* BODY */}
               <div className="relative p-6 flex-auto">{body}</div>
               {/* FOOTER */}
-              <div className="flex flex-col gap-2 p-6">
+              <div className="flex flex-col gap-1 p-6">
                 <div className="flex flex-row items-center gap-4 w-full">
                   {secondaryAction && secondaryActionLabel && (
                     <Button
@@ -96,6 +96,7 @@ const Modal: React.FC<ModalProps> = ({
                     onClick={handleSubmit}
                   />
                 </div>
+                {footer}
               </div>
             </div>
           </div>
