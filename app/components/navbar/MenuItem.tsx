dbd3a9f0c363feceae95ac73326b44c0e8245de7
Businge931 <jimmybusinge9@gmail.com>
2023-05-08 04:50:07 +0300

    feat: add menu item styles

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -2,8 +2,20 @@
 
 import React from "react";
 
-const MenuItem = () => {
-  return <div>MenuItem</div>;
+interface MenuItemProps {
+  onClick: () => void;
+  label: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
+  return (
+    <div
+      onClick={onClick}
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+    >
+      {label}
+    </div>
+  );
 };
 
 export default MenuItem;
