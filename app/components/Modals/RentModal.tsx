13843a0018ee680fd80240d8d4269f048610b4ca
Businge931 <jimmybusinge9@gmail.com>
2023-05-09 00:09:58 +0300

    feat: defined the category steps

---
+++ app/components/Modals/RentModal.tsx
@@ -0,0 +1,63 @@
+"use client";
+import React, { useMemo, useState } from "react";
+import Modal from "./Modal";
+import useRentModal from "@/app/hooks/useRentModal";
+import Heading from "../Heading";
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INF0 = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
+
+const RentModal = () => {
+  const [step, setStep] = useState(STEPS.CATEGORY);
+  const rentModal = useRentModal();
+
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+    return "Back";
+  }, [step]);
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which of these best describes your place?"
+        subtitle="Pick a category"
+      />
+    </div>
+  );
+
+  return (
+    <Modal
+      title="Airbnb your home!"
+      isOpen={rentModal.isOpen}
+      onClose={rentModal.onClose}
+      onSubmit={rentModal.onClose}
+      actionLabel={actionLabel}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      secondaryActionLabel={secondaryActionLabel}
+    />
+  );
+};
+
+export default RentModal;
